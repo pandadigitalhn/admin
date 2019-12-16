920effb3f4907dd9ccdbec25833e4507cf236982
Dennis Menjivar <demenjivar@tempoe.com>
2019-12-16 17:45:00 -0600

    Form filter step added

--- src/app/main/apps/calendar/calendar.component.ts
+++ src/app/main/apps/calendar/calendar.component.ts
@@ -1,273 +1,80 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { MatDialog, MatDialogRef } from '@angular/material';
-import { Subject } from 'rxjs';
-import { startOfDay, isSameDay, isSameMonth } from 'date-fns';
-import { CalendarEvent, CalendarEventAction, CalendarEventTimesChangedEvent, CalendarMonthViewDay } from 'angular-calendar';
 
-import { FuseConfirmDialogComponent } from '@fuse/components/confirm-dialog/confirm-dialog.component';
-import { fuseAnimations } from '@fuse/animations';
-
-import { CalendarService } from 'app/main/apps/calendar/calendar.service';
-import { CalendarEventModel } from 'app/main/apps/calendar/event.model';
-import { CalendarEventFormDialogComponent } from 'app/main/apps/calendar/event-form/event-form.component';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subject, Observable } from 'rxjs';
+import { FormControl } from '@angular/forms';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
-    selector     : 'calendar',
-    templateUrl  : './calendar.component.html',
-    styleUrls    : ['./calendar.component.scss'],
-    encapsulation: ViewEncapsulation.None,
-    animations   : fuseAnimations
+    selector: 'calendar',
+    templateUrl: './calendar.component.html',
+    styleUrls: ['./calendar.component.scss']
 })
-export class CalendarComponent implements OnInit
-{
-    actions: CalendarEventAction[];
-    activeDayIsOpen: boolean;
-    confirmDialogRef: MatDialogRef<FuseConfirmDialogComponent>;
-    dialogRef: any;
-    events: CalendarEvent[];
-    refresh: Subject<any> = new Subject();
-    selectedDay: any;
-    view: string;
-    viewDate: Date;
+export class CalendarComponent implements OnInit {
 
-    constructor(
-        private _matDialog: MatDialog,
-        private _calendarService: CalendarService
-    )
-    {
-        // Set the defaults
-        this.view = 'month';
-        this.viewDate = new Date();
-        this.activeDayIsOpen = true;
-        this.selectedDay = {date: startOfDay(new Date())};
+    // Vertical Stepper
+    verticalStepperStep1: FormGroup;
+    verticalStepperStep2: FormGroup;
+    verticalStepperStep3: FormGroup;
 
-        this.actions = [
-            {
-                label  : '<i class="material-icons s-16">edit</i>',
-                onClick: ({event}: { event: CalendarEvent }): void => {
-                    this.editEvent('edit', event);
-                }
-            },
-            {
-                label  : '<i class="material-icons s-16">delete</i>',
-                onClick: ({event}: { event: CalendarEvent }): void => {
-                    this.deleteEvent(event);
-                }
-            }
-        ];
-
-        /**
-         * Get events from service/server
-         */
-        this.setEvents();
-    }
-
-    // -----------------------------------------------------------------------------------------------------
-    // @ Lifecycle hooks
-    // -----------------------------------------------------------------------------------------------------
+    // Private
+    private _unsubscribeAll: Subject<any>;
 
     /**
-     * On init
-     */
-    ngOnInit(): void
-    {
-        /**
-         * Watch re-render-refresh for updating db
-         */
-        this.refresh.subscribe(updateDB => {
-            if ( updateDB )
-            {
-                this._calendarService.updateEvents(this.events);
-            }
-        });
-
-        this._calendarService.onEventsUpdated.subscribe(events => {
-            this.setEvents();
-            this.refresh.next();
-        });
-    }
-
-    // -----------------------------------------------------------------------------------------------------
-    // @ Public methods
-    // -----------------------------------------------------------------------------------------------------
-
-    /**
-     * Set events
-     */
-    setEvents(): void
-    {
-        this.events = this._calendarService.events.map(item => {
-            item.actions = this.actions;
-            return new CalendarEventModel(item);
-        });
-    }
-
-    /**
-     * Before View Renderer
+     * Constructor
      *
-     * @param {any} header
-     * @param {any} body
+     * @param {FormBuilder} _formBuilder
      */
-    beforeMonthViewRender({header, body}): void
-    {
-        /**
-         * Get the selected day
-         */
-        const _selectedDay = body.find((_day) => {
-            return _day.date.getTime() === this.selectedDay.date.getTime();
-        });
 
-        if ( _selectedDay )
-        {
-            /**
-             * Set selected day style
-             * @type {string}
-             */
-            _selectedDay.cssClass = 'cal-selected';
-        }
-
+    constructor(private _formBuilder: FormBuilder) {
+        // Set the private defaults
+        this._unsubscribeAll = new Subject();
     }
 
-    /**
-     * Day clicked
-     *
-     * @param {MonthViewDay} day
-     */
-    dayClicked(day: CalendarMonthViewDay): void
-    {
-        const date: Date = day.date;
-        const events: CalendarEvent[] = day.events;
+    myControl = new FormControl();
+    options: string[] = ['Telvista'];
+    filteredOptions: Observable<string[]>;
 
-        if ( isSameMonth(date, this.viewDate) )
-        {
-            if ( (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) || events.length === 0 )
-            {
-                this.activeDayIsOpen = false;
-            }
-            else
-            {
-                this.activeDayIsOpen = true;
-                this.viewDate = date;
-            }
-        }
-        this.selectedDay = day;
-        this.refresh.next();
-    }
-
-    /**
-     * Event times changed
-     * Event dropped or resized
-     *
-     * @param {CalendarEvent} event
-     * @param {Date} newStart
-     * @param {Date} newEnd
-     */
-    eventTimesChanged({event, newStart, newEnd}: CalendarEventTimesChangedEvent): void
-    {
-        event.start = newStart;
-        event.end = newEnd;
-        // console.warn('Dropped or resized', event);
-        this.refresh.next(true);
-    }
-
-    /**
-     * Delete Event
-     *
-     * @param event
-     */
-    deleteEvent(event): void
-    {
-        this.confirmDialogRef = this._matDialog.open(FuseConfirmDialogComponent, {
-            disableClose: false
+    ngOnInit(): void {
+        this.filteredOptions = this.myControl.valueChanges
+            .pipe(
+                startWith(''),
+                map(value => this._filter(value))
+            );
+        // Vertical Stepper form stepper
+        this.verticalStepperStep1 = this._formBuilder.group({
+            firstName: ['', Validators.required]
         });
 
-        this.confirmDialogRef.componentInstance.confirmMessage = 'Are you sure you want to delete?';
-
-        this.confirmDialogRef.afterClosed().subscribe(result => {
-            if ( result )
-            {
-                const eventIndex = this.events.indexOf(event);
-                this.events.splice(eventIndex, 1);
-                this.refresh.next(true);
-            }
-            this.confirmDialogRef = null;
+        this.verticalStepperStep2 = this._formBuilder.group({
+            address: ['', Validators.required]
         });
 
+        this.verticalStepperStep3 = this._formBuilder.group({
+            city: ['', Validators.required],
+            state: ['', Validators.required],
+            postalCode: ['', [Validators.required, Validators.maxLength(5)]]
+        });
     }
 
-    /**
-     * Edit Event
-     *
-     * @param {string} action
-     * @param {CalendarEvent} event
-     */
-    editEvent(action: string, event: CalendarEvent): void
-    {
-        const eventIndex = this.events.indexOf(event);
-
-        this.dialogRef = this._matDialog.open(CalendarEventFormDialogComponent, {
-            panelClass: 'event-form-dialog',
-            data      : {
-                event : event,
-                action: action
-            }
-        });
-
-        this.dialogRef.afterClosed()
-            .subscribe(response => {
-                if ( !response )
-                {
-                    return;
-                }
-                const actionType: string = response[0];
-                const formData: FormGroup = response[1];
-                switch ( actionType )
-                {
-                    /**
-                     * Save
-                     */
-                    case 'save':
-
-                        this.events[eventIndex] = Object.assign(this.events[eventIndex], formData.getRawValue());
-                        this.refresh.next(true);
-
-                        break;
-                    /**
-                     * Delete
-                     */
-                    case 'delete':
-
-                        this.deleteEvent(event);
-
-                        break;
-                }
-            });
+    ngOnDestroy(): void {
+        // Unsubscribe from all subscriptions
+        this._unsubscribeAll.next();
+        this._unsubscribeAll.complete();
     }
 
-    /**
-     * Add Event
-     */
-    addEvent(): void
-    {
-        this.dialogRef = this._matDialog.open(CalendarEventFormDialogComponent, {
-            panelClass: 'event-form-dialog',
-            data      : {
-                action: 'new',
-                date  : this.selectedDay.date
-            }
-        });
-        this.dialogRef.afterClosed()
-            .subscribe((response: FormGroup) => {
-                if ( !response )
-                {
-                    return;
-                }
-                const newEvent = response.getRawValue();
-                newEvent.actions = this.actions;
-                this.events.push(newEvent);
-                this.refresh.next(true);
-            });
+    finishHorizontalStepper(): void {
+        alert('You have finished the horizontal stepper!');
+    }
+
+    finishVerticalStepper(): void {
+        alert('You have finished the vertical stepper!');
+    }
+
+    private _filter(value: string): string[] {
+        const filterValue = value.toLowerCase();
+
+        return this.options.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
     }
 }
 
--- src/app/main/apps/calendar/calendar.module.ts
+++ src/app/main/apps/calendar/calendar.module.ts
@@ -1,58 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {
-    MatButtonModule, MatDatepickerModule, MatDialogModule, MatFormFieldModule, MatIconModule, MatInputModule, MatSlideToggleModule, MatToolbarModule, MatTooltipModule, MatSelectModule, MatStepperModule
-} from '@angular/material';
-import { ColorPickerModule } from 'ngx-color-picker';
-import { CalendarModule as AngularCalendarModule, DateAdapter } from 'angular-calendar';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule, MatSelectModule, MatStepperModule, MatAutocompleteModule } from '@angular/material';
 
 import { FuseSharedModule } from '@fuse/shared.module';
-import { FuseConfirmDialogModule } from '@fuse/components';
-
-import { CalendarComponent } from 'app/main/apps/calendar/calendar.component';
-import { CalendarService } from 'app/main/apps/calendar/calendar.service';
-import { CalendarEventFormDialogComponent } from 'app/main/apps/calendar/event-form/event-form.component';
-import { FormsComponent } from 'app/main/ui/forms/forms.component';
+import { CalendarComponent } from './calendar.component';
 
 const routes: Routes = [
     {
-        path     : '**',
-        component: CalendarComponent,
-        children : [],
-        resolve  : {
-            chat: CalendarService
-        }
+        path: '**',
+        component: CalendarComponent
     }
 ];
 
 @NgModule({
-    declarations   : [
-        CalendarComponent,
-        CalendarEventFormDialogComponent
+    declarations: [
+        CalendarComponent
     ],
-    imports        : [
+    imports: [
         RouterModule.forChild(routes),
-
-        MatButtonModule,
-        MatDatepickerModule,
-        MatDialogModule,
-        MatFormFieldModule,
-        MatIconModule,
-        MatInputModule,
-        MatSlideToggleModule,
-        MatToolbarModule,
-        MatTooltipModule,
-
-        AngularCalendarModule.forRoot({
-            provide   : DateAdapter,
-            useFactory: adapterFactory
-        }),
-        ColorPickerModule,
-
-        FuseSharedModule,
-        FuseConfirmDialogModule,
-        ,
 
         MatButtonModule,
         MatFormFieldModule,
@@ -60,16 +25,13 @@ const routes: Routes = [
         MatInputModule,
         MatSelectModule,
         MatStepperModule,
-
         FuseSharedModule,
+        MatAutocompleteModule
     ],
-    providers      : [
-        CalendarService
+    providers: [
     ],
     entryComponents: [
-        CalendarEventFormDialogComponent
     ]
 })
-export class CalendarModule
-{
+export class CalendarModule {
 }
